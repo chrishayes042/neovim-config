33d129e7fe3a27ddfec40af8fb7c85617afe634b
Chris <chrishayes042@gmail.com>
2023-09-08 20:51:01 -0600

    neovim config

---
+++ init.lua
@@ -0,0 +1,11 @@
+--[[ init.lua ]]
+require('chris.core')
+require('chris.lazy')
+vim.wo.number = true
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtab = 4
+vim.o.shiftwidth = 4
+vim.wo.relativenumber = true    
+
+
---
+++ lua/chris/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+
+	"bluz71/vim-nightfly-guicolors",
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme nightfly]])
+	end,
+}
